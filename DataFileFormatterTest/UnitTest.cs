5a071db9280deec68e2c0228e921d4b493cbd2ef
yoshida16729438 <yoshida16729438@gmail.com>
2025-05-05 16:39:21 +0900

    try to make test pass on github actions

--- DataFileFormatterTest/UnitTest.cs
+++ DataFileFormatterTest/UnitTest.cs
@@ -11,37 +11,37 @@ namespace DataFileFormatterTest {
     [TestClass]
     public class UnitTest {
 
-        //[TestMethod]
-        //public async Task WhenValidJsonRequestedFromStdin() {
-        //    string output = TestContextHandler.GetOutputFilePath("formatted.json");
-        //    string[] props = new string[] { "--json", "--format", "--charset", "shift-jis", "--indentSpacesCount", "4", "--outfile", output };
+        [TestMethod]
+        public async Task WhenValidJsonRequestedFromStdin() {
+            string output = TestContextHandler.GetOutputFilePath("formatted.json");
+            string[] props = new string[] { "--json", "--format", "--charset", "shift-jis", "--indentSpacesCount", "4", "--outfile", output };
 
-        //    using (MemoryStream ms = new MemoryStream()) {
-        //        Encoding sjis = Encoding.GetEncoding("shift-jis");
-        //        foreach (string line in File.ReadAllLines(TestContextHandler.GetTestDataPath("unindented.json"), Encoding.GetEncoding("UTF-8"))) {
-        //            byte[] data = sjis.GetBytes(line);
-        //            ms.Write(data, 0, data.Length);
-        //        }
-        //        ms.Position = 0;
+            using (MemoryStream ms = new MemoryStream()) {
+                Encoding sjis = Encoding.GetEncoding("shift-jis");
+                foreach (string line in File.ReadAllLines(TestContextHandler.GetTestDataPath("unindented.json"), Encoding.GetEncoding("UTF-8"))) {
+                    byte[] data = sjis.GetBytes(line);
+                    ms.Write(data, 0, data.Length);
+                }
+                ms.Position = 0;
 
-        //        using (TextReader sr = new StreamReader(ms)) {
-        //            Console.SetIn(sr);
+                using (TextReader sr = new StreamReader(ms)) {
+                    Console.SetIn(sr);
 
-        //            // to mark as redirected
-        //            SetConsoleRedirect(true);
+                    // to mark as redirected
+                    SetConsoleRedirect(true);
 
-        //            int result = await Program.Main(props);
-        //            Assert.AreEqual(0, result);
+                    int result = await Program.Main(props);
+                    Assert.AreEqual(0, result);
 
-        //            //reset redirected
-        //            SetConsoleRedirect(false);
-        //        }
-        //    }
+                    //reset redirected
+                    SetConsoleRedirect(false);
+                }
+            }
 
-        //    string expected = File.ReadAllText(TestContextHandler.GetTestDataPath("indentWithFourSpaces.json"));
-        //    string actual = File.ReadAllText(output);
-        //    Assert.AreEqual(expected, actual);
-        //}
+            string expected = File.ReadAllText(TestContextHandler.GetTestDataPath("indentWithFourSpaces.json"));
+            string actual = File.ReadAllText(output);
+            Assert.AreEqual(expected, actual);
+        }
 
         [TestMethod]
         public async Task NoInputDataSpecified() {
